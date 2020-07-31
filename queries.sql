06a07e9c37ae5b97eabd77de78b5eb942af9c5ce
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-07-31 17:36:22 -0400

    updated query file and created employee info csv.

--- queries.sql
+++ queries.sql
@@ -86,3 +86,31 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+-- 7. List 1: Create employee/salary information list and export as csv.
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+--
+SELECT emp_no, first_name, last_name, gender
+--INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--
+SELECT e.emp_no, 
+	   e.first_name, 
+	   e.last_name, 
+	   e.gender,
+	   s.salary,
+	   de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+-- 8. List 2: Create management list and export as csv.
